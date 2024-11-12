4e807da015dd2420f2396dcf9e11d0fe1c0482dc
Andrew Show <andrew_show@hotmail.com>
2024-11-12 12:00:10 +0800

    Use setup-lsp to generate compile database

--- build.sh
+++ build.sh
@@ -1,37 +1,50 @@
-#!/bin/sh
+#!/bin/bash
 
-# download and extract source code
-wget https://github.com/Stichting-MINIX-Research-Foundation/minix/archive/refs/tags/v3.3.0.tar.gz
-tar xvf v3.3.0.tar.gz
+devel=no
+if [ "X$1" == "Xdevel" ]; then
+    devel=yes
+fi
+build_sh=$(realpath -e $0)
 
-mv minix-3.3.0 src
+if [ ! -d src ]; then
+    # download and extract source code
+    wget https://github.com/Stichting-MINIX-Research-Foundation/minix/archive/refs/tags/v3.3.0.tar.gz
+    tar xvf v3.3.0.tar.gz
 
-# apply patches to fix build issue
-cd src
-cat ../patches/*.patch | patch -p1
+    mv minix-3.3.0 src
 
-# build tools and minix
-export JOBS=8
-./releasetools/x86_hdimage.sh
+    # apply patches to fix build issue
+    cd src
+    cat ../patches/*.patch | patch -p1
+else
+    cd src
+fi
 
-# rebuild kenel with debug enabled
-export PATH=$(cd ../obj.i386/tooldir.*/bin && pwd):$PATH
+if [ ! -d ../obj.i386 ]; then
+    # build tools and minix
+    export JOBS=8
+    ./releasetools/x86_hdimage.sh
+fi
 
-export DBG='-g -O0'
+if [ $devel == "yes" ]; then
+    # rebuild kenel with debug enabled
+    export PATH=$(cd ../obj.i386/tooldir.*/bin && pwd):$PATH
 
-echo '[' > compile_commands.sh
-echo ']' > compile_commands.sh
+    export DBG='-g -O0'
 
-for i in minix/kernel minix/fs minix/net minix/servers minix/drivers; do
-    nbmake-i386 -C $i clean
-    nbmake-i386 -C $i
-    setup-lsp.sh -a nbmake-i386 -C $i install
-done
+    for i in minix/kernel minix/fs minix/net minix/servers minix/drivers; do
+        nbmake-i386 -C $i clean
+        nbmake-i386 -C $i
+        nbmake-i386 -C $i install
+    done
+else
+    setup-lsp.sh -p i586-elf32-minix- -C .. $build_sh devel
 
-nbmake-i386 -C releasetools hdboot
+    nbmake-i386 -C releasetools hdboot
 
-# create image
-export CREATE_IMAGE_ONLY=1
-./releasetools/x86_hdimage.sh
+    # create image
+    export CREATE_IMAGE_ONLY=1
+    ./releasetools/x86_hdimage.sh
+fi
 
 cd ..
