7f06e4648e2897b15f7d8df89e2f1db4986a0fd0
Andrew Show <andrew_show@hotmail.com>
2024-11-12 17:06:19 +0800

    Add initial command when start gdb

--- gdb-run.sh
+++ gdb-run.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
 
-gdb -ex='target remote localhost:1234' $PWD/obj.i386/destdir.i386/boot/minix/.temp/kernel
+GDBINIT=/tmp/.gdbinit-$$
+
+cat > $GDBINIT <<EOF
+file $PWD/obj.i386/destdir.i386/boot/minix/.temp/kernel
+target remote localhost:1234
+add-symbol-file $PWD/obj.i386/destdir.i386/boot/minix/.temp/mod01_ds
+add-symbol-file $PWD/obj.i386/destdir.i386/boot/minix/.temp/mod02_rs
+add-symbol-file $PWD/obj.i386/destdir.i386/boot/minix/.temp/mod03_pm
+add-symbol-file $PWD/obj.i386/destdir.i386/boot/minix/.temp/mod04_sched
+add-symbol-file $PWD/obj.i386/destdir.i386/boot/minix/.temp/mod05_vfs
+add-symbol-file $PWD/obj.i386/destdir.i386/boot/minix/.temp/mod06_memory
+add-symbol-file $PWD/obj.i386/destdir.i386/boot/minix/.temp/mod07_tty
+add-symbol-file $PWD/obj.i386/destdir.i386/boot/minix/.temp/mod08_mfs
+add-symbol-file $PWD/obj.i386/destdir.i386/boot/minix/.temp/mod09_vm
+add-symbol-file $PWD/obj.i386/destdir.i386/boot/minix/.temp/mod10_pfs
+add-symbol-file $PWD/obj.i386/destdir.i386/boot/minix/.temp/mod11_init
+br pre_init
+layout src
+c
+EOF
+
+gdb -x $GDBINIT
+
+rm $GDBINIT
