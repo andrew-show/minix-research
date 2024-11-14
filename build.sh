9f140f25124db0dc4edea835c6dfc62f08f7cb07
Andrew Show <andrew_show@hotmail.com>
2024-11-14 21:02:16 +0800

    Minor change

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ devel=no
 if [ "X$1" == "Xdevel" ]; then
     devel=yes
 fi
-build_sh=$(realpath -e $0)
+build_sh=$0
 
 if [ ! -d src ]; then
     # download and extract source code
@@ -16,35 +16,34 @@ if [ ! -d src ]; then
     # apply patches to fix build issue
     cd src
     cat ../patches/*.patch | patch -p1
-else
-    cd src
+    cd ..
 fi
 
-if [ ! -d ../obj.i386 ]; then
+if [ ! -d ./obj.i386 ]; then
     # build tools and minix
     export JOBS=8
+    cd src
     ./releasetools/x86_hdimage.sh
+    cd ..
 fi
 
 if [ $devel == "yes" ]; then
     # rebuild kenel with debug enabled
-    export PATH=$(cd ../obj.i386/tooldir.*/bin && pwd):$PATH
-
+    export PATH=$PWD/obj.i386/tooldir.`uname -s`-`uname -r`-`uname -m`/bin:$PATH
     export DBG='-g -O0'
 
     for i in minix/lib minix/kernel minix/fs minix/net minix/servers minix/drivers; do
-        nbmake-i386 -C $i clean
-        nbmake-i386 -C $i
-        nbmake-i386 -C $i install
+        nbmake-i386 -C src/$i clean
+        nbmake-i386 -C src/$i
+        nbmake-i386 -C src/$i install
     done
-else
-    setup-lsp.sh -p i586-elf32-minix- -C .. $build_sh devel
 
-    nbmake-i386 -C releasetools hdboot
+    nbmake-i386 -C src/releasetools hdboot
+else
+    setup-lsp.sh -p i586-elf32-minix- $build_sh devel
 
     # create image
-    export CREATE_IMAGE_ONLY=1
-    ./releasetools/x86_hdimage.sh
+    cd src
+    CREATE_IMAGE_ONLY=1 ./releasetools/x86_hdimage.sh
+    cd ..
 fi
-
-cd ..
